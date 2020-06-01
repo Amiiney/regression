44687b81f0891c56e8b4cf351c833cb045fbe9ea
Amiiney <52346157+Amiiney@users.noreply.github.com>
2020-06-01 12:11:38 +0400

    Add files via upload

---
+++ r_visual.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+
+def msv1(data, thresh=20, color='black', edgecolor='black', width=15, height=3):
+    """
+    SOURCE: https://www.kaggle.com/amiiiney/price-prediction-regularization-stacking
+    """
+    
+    plt.figure(figsize=(width,height))
+    percentage=(data.isnull().mean())*100
+    percentage.sort_values(ascending=False).plot.bar(color=color, edgecolor=edgecolor)
+    plt.axhline(y=thresh, color='r', linestyle='-')
+    plt.title('Missing values percentage per column', fontsize=20, weight='bold' )
+    plt.text(len(data.isnull().sum()/len(data))/1.7, thresh+12.5, 'Columns with more than %s%s missing values' %(thresh, '%'), fontsize=12, color='crimson',
+         ha='left' ,va='top')
+    plt.text(len(data.isnull().sum()/len(data))/1.7, thresh - 5, 'Columns with less than %s%s missing values' %(thresh, '%'), fontsize=12, color='green',
+         ha='left' ,va='top')
+    plt.xlabel('Columns', size=15, weight='bold')
+    plt.ylabel('Missing values percentage')
+    plt.yticks(weight ='bold')
+    
+    return plt.show()
+
+def msv2(data, width=12, height=8, color=('silver', 'gold','lightgreen','skyblue','lightpink'), edgecolor='black'):
+    """
+    SOURCE: https://www.kaggle.com/amiiiney/price-prediction-regularization-stacking
+    """
+    fig, ax = plt.subplots(figsize=(width, height))
+
+    allna = (data.isnull().sum() / len(data))*100
+    tightout= 0.008*max(allna)
+    allna = allna.drop(allna[allna == 0].index).sort_values().reset_index()
+    mn= ax.barh(allna.iloc[:,0], allna.iloc[:,1], color=color, edgecolor=edgecolor)
+    ax.set_title('Missing values percentage per column', fontsize=15, weight='bold' )
+    ax.set_xlabel('Percentage', weight='bold', size=15)
+    ax.set_ylabel('Features with missing values', weight='bold')
+    plt.yticks(weight='bold')
+    plt.xticks(weight='bold')
+    for i in ax.patches:
+        ax.text(i.get_width()+ tightout, i.get_y()+0.1, str(round((i.get_width()), 2))+'%',
+            fontsize=10, fontweight='bold', color='grey')
+    return plt.show()
+
